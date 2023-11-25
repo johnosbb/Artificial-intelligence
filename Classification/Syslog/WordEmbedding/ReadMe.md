0538086003091d82d5add0a46abedba5936bbc02
johnosbb <johnos37@yahoo.com>
2023-11-25 10:32:40 +0000

    Adding embedded example

---
+++ Classification/Syslog/WordEmbedding/ReadMe.md
@@ -0,0 +1,221 @@
+# Sentence Embedding
+
+## Overview
+
+![Flow Diagram](./sentence_embedding.svg)
+
+
+
+The encoding performed by the SentenceTransformer model is based on contextualized embeddings of entire sentences, not individual words. The model uses a transformer architecture, which processes input sequences as a whole, taking into account the relationships and dependencies between words in the context of the entire sentence.
+
+The process of sentence embedding involves a number of steps:
+
+- Tokenization: The input sentence is tokenized into subword or word-level tokens. Each token is associated with an embedding vector.
+- Embedding Layer: The embeddings of the individual tokens are passed through an embedding layer to obtain initial representations.
+- Positional Encoding: The model incorporates information about the position of tokens in the sequence to handle the sequential nature of the input. This is achieved by adding positional encoding to the token embeddings.
+- Transformer Blocks: The processed embeddings are then passed through multiple transformer blocks. Each block contains attention mechanisms that allow the model to focus on different parts of the input sequence when generating representations.
+- Contextualized Embeddings: The output of the transformer blocks is a set of contextualized embeddings for each token. These embeddings capture the relationships and contextual information of each token within the entire sentence.
+- Pooling or Aggregation: The model might use pooling or aggregation techniques to obtain a single vector representation for the entire sentence. This aggregated representation is often what is returned as the final encoding. There are a number of techniques that can be used for pooling and these include:
+  - Max Pooling: For each dimension of the word embeddings, the maximum value across all the words in the sequence is taken.
+This method captures the most salient features present in any word across the sequence.
+  - Mean Pooling: For each dimension of the word embeddings, the mean (average) value across all the words in the sequence is calculated. This method provides a representation that considers the overall distribution of features in the sequence.
+  - Sum Pooling: For each dimension of the word embeddings, the sum of values across all the words in the sequence is computed.
+This method emphasizes the cumulative effect of features present in different words.
+  - Weighted Pooling: Assigns different weights to word embeddings based on their importance. The weights can be learned during training or predefined based on some criterion. 
+  - CLS Token Pooling (BERT-style): In models like BERT (Bidirectional Encoder Representations from Transformers), a special token ([CLS]) is used to represent the entire sequence. The output of this [CLS] token is often used as the aggregated representation for the entire sentence.
+
+## Pretrained Model
+
+```txt
+all-mpnet-base-v2 
+Description:	All-round model tuned for many use-cases. Trained on a large and diverse dataset of over 1 billion training pairs.
+Base Model:	microsoft/mpnet-base
+Max Sequence Length:	384
+Dimensions:	768
+Normalized Embeddings:	true
+Suitable Score Functions:	dot-product (util.dot_score), cosine-similarity (util.cos_sim), euclidean distance
+Size:	420 MB
+Pooling:	Mean Pooling
+Training Data:	1B+ training pairs. For details, see model card.
+Model Card:	https://huggingface.co/sentence-transformers/all-mpnet-base-v2
+```
+
+
+
+
+## Classification
+
+For the syslog classification task a line in the log indicating an error would be  labelled 1, while a normal entry would be 0.
+
+
+Below is a sample of the syslog data. The comma separated values correspond to Data/Time, Application Name, Log Detail and Error Classification, where 1 indicates an error and 0 represents a normal log entry.
+
+```txt
+Date/Time,Application,Detail,Label
+Sep 14 09:00:04,systemd[1]:,Starting Time & Date Service...,0
+Sep 14 09:00:04,dbus-daemon[678]:,[system] Successfully activated service 'org.freedesktop.timedate1',0
+Sep 14 09:00:04,systemd[1]:,Started Time & Date Service.,0
+```
+
+## Encoding of Syslog using SentenceTransformer
+
+The encode method uses the pre-trained model (all-mpnet-base-v1 in this case) to convert each sentence in the provided list into a numerical embedding.  These embeddings are high-dimensional vectors that represent the semantic content of the corresponding sentences. The dimensionality of these embeddings is determined by the architecture of the pre-trained model. The model considers the entire sentence context when generating embeddings for each word/token in that sentence. This is in contrast to simpler word embeddings like Word2Vec or GloVe, which typically generate fixed embeddings for individual words without considering the context in which the words appear.
+
+## Performance
+
+```txt
+X train: (27931, 768), y train: (27931,)
+X test: (27932, 768), y test: (27932,)
+X_train.shape[1] = 768
+Epoch 0, Loss: 55.1343732252717
+Epoch 1, Loss: 17.644796123728156
+Epoch 2, Loss: 13.494277570396662
+Epoch 3, Loss: 11.013798132538795
+Epoch 4, Loss: 9.222700340207666
+Epoch 5, Loss: 8.078088800888509
+Epoch 6, Loss: 7.271093164104968
+Epoch 7, Loss: 6.797037958633155
+Epoch 8, Loss: 4.007764256559312
+Epoch 9, Loss: 3.136717396089807
+Epoch 10, Loss: 2.680896057980135
+Epoch 11, Loss: 2.3841857779189013
+Epoch 12, Loss: 2.100493803387508
+Epoch 13, Loss: 1.8955467828200199
+Epoch 14, Loss: 1.7078217276139185
+Epoch 15, Loss: 1.535166842630133
+Epoch 16, Loss: 1.3976086292532273
+Epoch 17, Loss: 1.2710987314058002
+Epoch 18, Loss: 1.1519548384967493
+Epoch 19, Loss: 1.0361130807286827
+Epoch 20, Loss: 0.948123012072756
+Epoch 21, Loss: 0.8619368533836678
+Epoch 22, Loss: 0.777278650275548
+Epoch 23, Loss: 0.7121596332654008
+Epoch 24, Loss: 0.6542467467515962
+Epoch 25, Loss: 0.5979286803049035
+Epoch 26, Loss: 0.5419136075070128
+Epoch 27, Loss: 0.492881482525263
+Epoch 28, Loss: 0.4474994336924283
+Epoch 29, Loss: 0.4196242537800572
+Epoch 30, Loss: 0.37453314586127817
+Epoch 31, Loss: 0.3433683945913799
+Epoch 32, Loss: 0.3053473650707019
+Epoch 33, Loss: 0.2761968898048508
+Epoch 34, Loss: 0.24772902307086042
+Epoch 35, Loss: 0.22381159995347844
+Epoch 36, Loss: 0.20461185132262472
+Epoch 37, Loss: 0.18168303904712957
+Epoch 38, Loss: 0.16358696337738365
+Epoch 39, Loss: 0.15059183884659433
+Epoch 40, Loss: 0.13129789802087544
+Epoch 41, Loss: 0.1177742621039215
+Epoch 42, Loss: 0.10614995019750495
+Epoch 43, Loss: 0.0973467846870335
+Epoch 44, Loss: 0.08591146249000303
+Epoch 45, Loss: 0.07985341559151493
+Epoch 46, Loss: 0.07107867492322839
+Epoch 47, Loss: 0.06659575940011564
+Epoch 48, Loss: 0.0577508631015462
+Epoch 49, Loss: 0.052158719968701917
+Epoch 50, Loss: 0.04716082357663254
+Epoch 51, Loss: 0.04397293565716609
+Epoch 52, Loss: 0.04039344753577723
+Epoch 53, Loss: 0.035663988881651676
+Epoch 54, Loss: 0.032795560925251266
+Epoch 55, Loss: 0.02931291885272458
+Epoch 56, Loss: 0.028394002232744242
+Epoch 57, Loss: 0.024599164915571237
+Epoch 58, Loss: 0.022294904512648372
+Epoch 59, Loss: 0.02021452816510294
+Epoch 60, Loss: 0.018884399346234204
+Epoch 61, Loss: 0.017847621654851764
+Epoch 62, Loss: 0.016730387970170568
+Epoch 63, Loss: 0.015163953869489433
+Epoch 64, Loss: 0.014623393211138591
+Epoch 65, Loss: 0.01237590216959461
+Epoch 66, Loss: 0.011729023164321006
+Epoch 67, Loss: 0.010891874875028407
+Epoch 68, Loss: 0.010187782129094103
+Epoch 69, Loss: 0.009378529113632794
+Epoch 70, Loss: 0.008623588166386753
+Epoch 71, Loss: 0.008054979974911447
+Epoch 72, Loss: 0.0076901299428762115
+Epoch 73, Loss: 0.007406980978657884
+Epoch 74, Loss: 0.006882498012828364
+Epoch 75, Loss: 0.006406679874416454
+Epoch 76, Loss: 0.00611464562076236
+Epoch 77, Loss: 0.005550848914111839
+Epoch 78, Loss: 0.005432851446570908
+Epoch 79, Loss: 0.0049441754502197455
+Epoch 80, Loss: 0.004776179216106957
+Epoch 81, Loss: 0.004507060493338599
+Epoch 82, Loss: 0.004240301953206682
+Epoch 83, Loss: 0.00403994900512572
+Epoch 84, Loss: 0.003920902846253682
+Epoch 85, Loss: 0.0037100705596149197
+Epoch 86, Loss: 0.0035048500802687954
+Epoch 87, Loss: 0.0032915347888149427
+Epoch 88, Loss: 0.003212780365386081
+Epoch 89, Loss: 0.0030598549444247336
+Epoch 90, Loss: 0.002990219903310276
+Epoch 91, Loss: 0.00293181798595632
+Epoch 92, Loss: 0.0029842452374850836
+Epoch 93, Loss: 0.0027424828739004425
+Epoch 94, Loss: 0.0026776573045745877
+Epoch 95, Loss: 0.002520326592396316
+Epoch 96, Loss: 0.0024364478833618364
+Epoch 97, Loss: 0.002370084128971328
+Epoch 98, Loss: 0.0022932237187047733
+Epoch 99, Loss: 0.002195303940219162
+
+-----------------------------------
+
+The accuracy of the model is 100.0%.
+Naive Classifier: 97.3 %
+
+```
+
+## Loss Curve
+
+![Confusion Matrix](./loss_plot.svg)
+
+
+
+## Confusion Matrix
+
+![Confusion Matrix](./confusion_matrix.png)
+
+The above confusion matrix indicates 0 type 1 erros (False Positives) and 14 type 2 errors (False Negatives)
+
+## Receiver Operating Characteristic (ROC) curves
+
+The Receiver Operating Characteristic (ROC) curve is a graphical representation of the performance of a binary classification model at various classification thresholds. It is a widely used tool for evaluating the trade-off between the True Positive Rate (Sensitivity) and the False Positive Rate at different decision thresholds.
+
+True Positive Rate:
+
+```txt
+TPR = True Positives/ (False Negatives + True Positives)
+``` 
+
+The True Positive Rate measures the proportion of actual positive instances that are correctly predicted as positive. It is also known as Sensitivity or Recall.  A high TPR indicates that the model is effective at capturing positive instances.
+
+
+False Positive Rate: 
+
+```txt
+FPR = False Positives/ (False Positives + True Negatives)
+``` 
+
+The False Positive Rate measures the proportion of actual negative instances that are incorrectly predicted as positive. A low FPR indicates that the model is not producing many false positives.
+
+ROC curves are particularly useful for comparing and selecting models. A model with a curve that is closer to the upper-left corner (higher TPR and lower FPR) generally performs better.
+
+The ROC curve helps in selecting an appropriate classification threshold based on the specific requirements of the application. Depending on the use case, you may prioritize sensitivity (minimizing false negatives) or specificity (minimizing false positives).
+
+![ROC Plot](./roc_plot.svg)
+
+Area Under the Curve (AUC):
+
+The AUC-ROC metric quantifies the overall performance of the model. A higher AUC value (closer to 1) indicates better discrimination between positive and negative instances across various thresholds. However, area under the curve as a means of comparison should be evaluated with caution depending on the relative importance of a false positive versus a false negative.
+
+
--- Classification/Syslog/WordEmbedding/syslog_embeddings.py
+++ Classification/Syslog/WordEmbedding/syslog_embeddings.py
@@ -1,46 +1,69 @@
-import pandas as pd
+
 import numpy as np
+import pandas as pd
+import pickle
 from collections import Counter
-from sklearn.feature_extraction.text import CountVectorizer
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve,auc
-import joblib
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
-import seaborn as sns
-import matplotlib.pyplot as plt
+from sentence_transformers import SentenceTransformer # is a Python library that provides pre-trained models for generating numerical representations (embeddings) of sentences. These embeddings are designed to capture the semantic meaning of sentences, allowing for various natural language processing (NLP) tasks such as text similarity, clustering, and classification.
 
-# Reference: https://jeancochrane.com/blog/pytorch-functional-api
 
 syslog_file = '../data/syslog.csv'
 df = pd.read_csv(syslog_file).dropna()
 
 BATCH_SIZE = 512 # smaller batch size produces lower generalization errors. Is is easier to avoid constraints with memory or GPU.
 NUM_EPOCHS = 100
-
+SYSLOG_PICKLE="../model/syslog_X.pkl"
 X = df['Detail'].values
 y = df['Label'].values
 
+
+BATCH_SIZE = 128
+NUM_EPOCHS = 100 # Overfitting: Continuing training for too many epochs can lead to overfitting. Overfitting occurs when a model becomes too specialized in the training data and doesn't generalize well to new, unseen data. If the loss on the training set continues to decrease while the loss on a validation set (or new data) starts to increase, it's a sign of overfitting.
+MAX_FEATURES = 10
+CREATE_ENCODINGS = False
+
+# https://www.sbert.net/
+emb_model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2') # all-mpnet-base-v1': This is specifying the specific pre-trained model to be loaded. In this case, it's using the model named all-mpnet-base-v1 from the sentence-transformers library. The all-mpnet-base-v1 model is based on the MPNet (Multilingual Pre-trained BERT) architecture.
+
+# sentences = [ "Each sentence is converted"]
+# embeddings = emb_model.encode(sentences)
+# print(embeddings.squeeze().shape)
+
+# The encode method uses the pre-trained model (all-mpnet-base-v1 in this case) to convert each sentence in the provided
+# list into a numerical embedding.
+# These embeddings are high-dimensional vectors that represent the semantic content of the corresponding sentences.
+# The dimensionality of these embeddings is determined by the architecture of the pre-trained model.
+# The model considers the entire sentence context when generating embeddings for each word/token in that sentence.
+# This is in contrast to simpler word embeddings like Word2Vec or GloVe, 
+# which typically generate fixed embeddings for individual words without considering the context in which the words appear.
+
+if (CREATE_ENCODINGS):
+    X = emb_model.encode(df['Detail'].values) #This method takes a list of strings (or a single string) and returns the embeddings (numerical representations) for those sentences.
+    with open(SYSLOG_PICKLE, "wb") as output_file:
+        pickle.dump(X, output_file)
+else:
+    with open(SYSLOG_PICKLE, "rb") as input_file:
+        X = pickle.load(input_file)
+
+
+
+# split the data into training and test data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5, random_state=123)
 print(f"X train: {X_train.shape}, y train: {y_train.shape}\nX test: {X_test.shape}, y test: {y_test.shape}")
 
-one_hot = CountVectorizer() # convert a collection of text documents to a matrix of token counts.  Each row in the matrix corresponds to a document, and each column corresponds to a unique word (token) in the entire collection of documents.
-X_train_onehot = one_hot.fit_transform(X_train) #  learn the parameters from the training data and transform that data.
-X_test_onehot = one_hot.transform(X_test) # just transform the test data
-
-number_of_features = X_train_onehot.shape[1]
-print(f"X_train_onehot.shape[1] = {number_of_features}")
-
-# Save the CountVectorizer from the training stage
-joblib.dump(one_hot, '../models/count_vectorizer.joblib')
 
 
-class SyslogData(Dataset): # inherits the Dataset Class
+class SyslogData(Dataset):
     def __init__(self, X, y):
         super().__init__()
-        self.X = torch.Tensor(X.toarray())
+        self.X = torch.Tensor(X)
         self.y = torch.Tensor(y).type(torch.LongTensor)
         self.len = len(self.X)
         
@@ -51,24 +74,28 @@ class SyslogData(Dataset): # inherits the Dataset Class
         return self.X[index], self.y[index]
     
 
-train_ds = SyslogData(X= X_train_onehot, y = y_train)
-test_ds = SyslogData(X_test_onehot, y_test)
+train_ds = SyslogData(X= X_train, y = y_train)
+test_ds = SyslogData(X_test, y_test)
 
 train_loader = DataLoader(dataset=train_ds, batch_size=BATCH_SIZE, shuffle=True)
-test_loader = DataLoader(test_ds, batch_size=30000) # 27932 is a factor of the total size of the data. In this case there are (27932 * 2 = 55864) samples and we use half for test
+test_loader = DataLoader(test_ds, batch_size=30000)
+
+number_of_features = X_train.shape[1]
+print(f"X_train.shape[1] = {number_of_features}")
+
 
 class SyslogModel(nn.Module):
-    def __init__(self, NUM_FEATURES, NUM_CLASSES, HIDDEN = 10, loss_function=None, optimizer=None):
+    def __init__(self, NUM_FEATURES, NUM_CLASSES, HIDDEN = 10,loss_function=None, optimizer=None):
         super().__init__()
-        self.linear = nn.Linear(NUM_FEATURES, HIDDEN) # nn.Linear is a linear transformation layer, also known as a fully connected or dense layer. NUM_FEATURES represents the number of input features (neurons) to this layer. HIDDEN represents the number of neurons in the hidden layer.
+        self.linear = nn.Linear(NUM_FEATURES, HIDDEN)
         self.linear2 = nn.Linear(HIDDEN, NUM_CLASSES)
-        self.relu = nn.ReLU() # nn.ReLU is the rectified linear unit activation function. It introduces non-linearity to the model by outputting the input directly if it is positive; otherwise, it will output zero.
-        self.log_softmax = nn.LogSoftmax(dim=1) # nn.LogSoftmax is used to convert the raw output scores from the output layer into log probabilities.
+        self.relu = nn.ReLU()
+        self.log_softmax = nn.LogSoftmax(dim=1)
         self.loss_function = loss_function
         self.optimizer_function = optimizer
-        
+    
     def make_prediction(self,x):
-        return self.forward(x)    
+            return self.forward(x) 
         
     def forward(self, x):
         x = self.linear(x)
@@ -76,7 +103,7 @@ class SyslogModel(nn.Module):
         x = self.linear2(x)
         x = self.log_softmax(x)
         return x
-    
+
     def zero_gradient(self):
         if self.optimizer is not None:
                 self.optimizer.zero_grad()
@@ -95,6 +122,7 @@ class SyslogModel(nn.Module):
             self.optimizer =  self.optimizer_function(self.parameters())
         else:
             raise ValueError("Optimizer not provided during initialization.")
+
 
 
 # Assuming y_true and y_score are your PyTorch tensors
@@ -126,49 +154,35 @@ def plot_roc(y_true,y_score):
     plt.title('Receiver Operating Characteristic (ROC) Curve')
     plt.legend(loc='lower right')
     #plt.show()
-    plt.savefig('roc_plot.svg', format='svg')
-
-
+    plt.savefig('roc_plot_embeddings.svg', format='svg')
+    
+    
 model = SyslogModel(NUM_FEATURES = number_of_features, NUM_CLASSES = 2,loss_function=nn.CrossEntropyLoss,optimizer=torch.optim.AdamW)
 model.initialize_optimizer()
 model.initialize_loss()
-
-# # nn.CrossEntropyLoss is a PyTorch loss function specifically designed for multiclass classification problems.
-# # It combines the softmax activation function and the negative log-likelihood loss.
-# # It expects raw logits (unnormalized scores) as input and internally applies the softmax function to convert
-# # them into probabilities before computing the loss.
-# criterion = nn.CrossEntropyLoss()
-# # torch.optim.AdamW is an implementation of the Adam optimizer with weight decay.
-# # AdamW is a variant of the Adam optimizer that includes L2 weight decay (weight regularization).
-# optimizer = torch.optim.AdamW(model.parameters())
 
 
 train_losses = []
 for e in range(NUM_EPOCHS):
     curr_loss = 0
-    for X_batch, y_batch in train_loader: # this will allow us to batch the data on each Epoch
+    for X_batch, y_batch in train_loader:
         # In PyTorch, optimizers know nothing about the training loop, 
         # so they will continue to accumulate gradients indefinitely unless instructed to stop.
         # Hence, to initiate a new forward and backward pass,
         # we need to start by clearing out any gradients that may have previously been calculated:
         model.zero_gradient()
         y_pred_log = model.make_prediction(X_batch) # this invokes the forward pass in the model
-        loss = model.loss(y_pred_log, y_batch.long()) # calculate our losses        
+        loss = model.loss(y_pred_log, y_batch.long()) # calculate our losses  
+        # Early stopping check     
         curr_loss += loss.item()
-        # With loss.backward() PyTorch is obscuring the fact that it is using the layers contained by model 
-        # to propagate the gradient backward through the network -- 
-        # layers which have been passed through to output by the loss function,
-        # in order to calculate the gradient in loss.backward().
-        # Instead of making this shared state clear, the API obscures it, 
-        # returning None and mutating gradient state in-place.
         loss.backward() # triggers automatic gradient calculation
         model.optimizer.step()  # Update the weights
     train_losses.append(curr_loss)
     print(f"Epoch {e}, Loss: {curr_loss}")
 
 loss_plot =  sns.lineplot(x=list(range(len(train_losses))), y= train_losses)
-loss_plot.figure.savefig('loss_plot.svg')
-#plt.show()
+loss_plot.figure.savefig('loss_plot_embeddings.svg')
+
 
 
 with torch.no_grad(): # This context manager is used to turn off gradient computation within its scope. It means any operations inside this block won't track gradients for subsequent backward passes.
@@ -179,22 +193,16 @@ with torch.no_grad(): # This context manager is used to turn off gradient comput
             y_test_pred = torch.argmax(y_test_pred_log, dim = 1)
             y_test_pred_total.extend(y_test_pred.cpu().numpy())
     len(y_test_pred_total)
-    # for X_batch, y_batch in test_loader:
-    #     y_test_pred_log = model.make_prediction(X_batch) # The model is used to make predictions 
-    #     y_test_pred = torch.argmax(y_test_pred_log, dim = 1) #  This line finds the indices of the maximum values along the specified dimension (dim=1), effectively giving the predicted class labels. The result, y_test_pred, contains the predicted labels for the batch.
 
-y_test_pred_np = y_test_pred.squeeze().cpu().numpy() # This removes dimensions of size 1 from the shape of y_test_pred. It is commonly used to remove unnecessary singleton dimensions.
-print(f"y_test_pred_np.shape {y_test_pred_np.shape}  y_test_pred {y_test_pred.shape}")
-print(f"y_test.shape {y_test.shape}")
-acc = accuracy_score(y_pred=y_test_pred_total, y_true = y_test)
+
+y_test_pred_np = y_test_pred.squeeze().cpu().numpy()
+
+acc = accuracy_score(y_pred=y_test_pred_np, y_true = y_test)
 print(f"The accuracy of the model is {np.round(acc, 3)*100}%.")
+
 most_common_cnt = Counter(y_test).most_common()[0][1]
 print(f"Naive Classifier: {np.round(most_common_cnt / len(y_test) * 100, 1)} %") # This line calculates the percentage of the dataset that corresponds to the most common class and rounds the result to one decimal place. It represents the accuracy of a very simple baseline classifier that always predicts the most common class. This baseline is often referred to as a "majority class classifier" or a "naive classifier."
 heatmap_plot = sns.heatmap(confusion_matrix(y_test_pred_np, y_test), annot=True, fmt=".0f")
 #plt.show()
-heatmap_plot.figure.savefig('heatmap.svg')
-
+heatmap_plot.figure.savefig('heatmap_embeddings.svg')
 plot_roc(y_test_pred_np,y_test)
-#sns.heatmap_plot.figure.savefig('heatmap.svg')
-# Save the trained model
-torch.save(model.state_dict(), f"../models/syslog_model_{number_of_features}.pth") # if we save the complete model we are dependent on directory structures. We just save the state dictionary. This has learnable parameters and registered buffers 
